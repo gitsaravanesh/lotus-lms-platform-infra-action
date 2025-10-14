c01dd962157cc4412f356c0eaa18ad493d168739
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-10-15 01:00:41 +0530

    trying to push

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   type    = string
   default = "ap-south-1"
 }
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  sensitive   = true
+}
