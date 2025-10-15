d05ba6ca53a96832fb2f30bfa2a1bc94d3c9d5d0
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-10-15 13:05:47 +0530

    chnages to remove keys

--- terraform/envs/dev/variables.tf
+++ terraform/envs/dev/variables.tf
@@ -3,16 +3,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  sensitive   = true
-}
-
 # Cognito / Google details — provide via terraform.tfvars or CI secrets
 variable "google_client_id" {
 type = string
