3e6273d9402e5d4d1a7ddf293c1750ab3f1469e1
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-10-15 13:55:14 +0530

    chnages to remove keys

--- terraform/envs/dev/modules/vpc/variables.tf
+++ terraform/envs/dev/modules/vpc/variables.tf
@@ -3,15 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
