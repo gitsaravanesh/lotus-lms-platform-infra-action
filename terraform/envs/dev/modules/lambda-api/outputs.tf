30e5e0f13bf1fe6c22e80de301bf9680aefac405
gitsaravanesh <cloudtech.trainer@gmail.com>
2025-11-11 09:36:15 +0530

    add videos list api

--- terraform/envs/dev/modules/lambda-api/outputs.tf
+++ terraform/envs/dev/modules/lambda-api/outputs.tf
@@ -10,3 +10,18 @@ output "lambda_function_name" {
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.courses.name
 }
+
+output "videos_bucket_name" {
+  description = "Name of the S3 bucket for video storage"
+  value       = aws_s3_bucket.videos_bucket.id
+}
+
+output "videos_table_name" {
+  description = "Name of the DynamoDB table for video metadata"
+  value       = aws_dynamodb_table.videos_table.name
+}
+
+output "backend_role_arn" {
+  description = "ARN of the backend IAM role"
+  value       = aws_iam_role.backend_role.arn
+}
