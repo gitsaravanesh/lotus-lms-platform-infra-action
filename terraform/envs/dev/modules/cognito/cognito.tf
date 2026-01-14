661391843800bd4d623ea0ad07e43a115538d447
Saravanesh <cloudtech.trainer@gmail.com>
2026-01-15 03:25:32 +0530

    changes to cognito

--- terraform/envs/dev/modules/cognito/cognito.tf
+++ terraform/envs/dev/modules/cognito/cognito.tf
@@ -1,8 +1,7 @@
-##########################################
-# Cognito User Pool Main Client
-##########################################
-
-resource "aws_cognito_user_pool" "main" {
+############################################
+# Cognito User Pool (SINGLE SOURCE OF TRUTH)
+############################################
+resource "aws_cognito_user_pool" "this" {
   name = "${var.project_name}-user-pool"
 
   auto_verified_attributes = ["email"]
@@ -15,14 +14,20 @@ resource "aws_cognito_user_pool" "main" {
   }
 }
 
-resource "aws_cognito_user_pool_domain" "main" {
+############################################
+# Cognito Domain (Hosted UI)
+############################################
+resource "aws_cognito_user_pool_domain" "this" {
   domain       = "${var.project_name}-auth"
-  user_pool_id = aws_cognito_user_pool.main.id
+  user_pool_id = aws_cognito_user_pool.this.id
 }
 
+############################################
+# Direct Auth App Client (Username/Password)
+############################################
 resource "aws_cognito_user_pool_client" "direct_auth" {
   name         = "${var.project_name}-direct-auth"
-  user_pool_id = aws_cognito_user_pool.main.id
+  user_pool_id = aws_cognito_user_pool.this.id
 
   generate_secret = false
 
@@ -31,16 +36,45 @@ resource "aws_cognito_user_pool_client" "direct_auth" {
     "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
+
+  prevent_user_existence_errors = "ENABLED"
 }
 
+############################################
+# Google Identity Provider
+############################################
+resource "aws_cognito_identity_provider" "google" {
+  user_pool_id  = aws_cognito_user_pool.this.id
+  provider_name = "Google"
+  provider_type = "Google"
+
+  provider_details = {
+    client_id         = var.google_client_id
+    client_secret     = var.google_client_secret
+    authorize_scopes  = "openid email profile"
+  }
+
+  attribute_mapping = {
+    email    = "email"
+    username = "sub"
+  }
+}
+
+############################################
+# OAuth / Hosted UI App Client
+############################################
 resource "aws_cognito_user_pool_client" "oauth" {
   name         = "${var.project_name}-oauth"
-  user_pool_id = aws_cognito_user_pool.main.id
+  user_pool_id = aws_cognito_user_pool.this.id
 
   generate_secret = false
 
   allowed_oauth_flows = ["code"]
-  allowed_oauth_scopes = ["openid", "email", "profile"]
+  allowed_oauth_scopes = [
+    "openid",
+    "email",
+    "profile"
+  ]
   allowed_oauth_flows_user_pool_client = true
 
   callback_urls = var.callback_urls
@@ -50,7 +84,8 @@ resource "aws_cognito_user_pool_client" "oauth" {
     "COGNITO",
     aws_cognito_identity_provider.google.provider_name
   ]
-  
+
+  # CRITICAL: Google must exist before OAuth client
   depends_on = [
     aws_cognito_identity_provider.google
   ]
